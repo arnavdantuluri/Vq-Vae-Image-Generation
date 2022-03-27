bc34941adb5c2e56b748d8fc12542501e7bf581e
Aldrich <74431077+arnavdantuluri@users.noreply.github.com>
2022-03-27 19:10:29 -0400

    Add files via upload

---
+++ Research/VAE/vae.py
@@ -0,0 +1,96 @@
+'''
+Simple Variational AutoEncoder to serve as a baseline
+'''
+
+
+# TensorFlow ≥2.0 is required
+import tensorflow as tf
+from tensorflow import keras
+
+# Common imports
+import numpy as np
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+K = keras.backend
+
+def rounded_accuracy(y_true, y_pred):
+    return keras.metrics.binary_accuracy(tf.round(y_true), tf.round(y_pred))
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
+X_train_full = X_train_full.astype(np.float32) / 255
+X_test = X_test.astype(np.float32) / 255
+X_train, X_valid = X_train_full[:-5000], X_train_full[-5000:]
+y_train, y_valid = y_train_full[:-5000], y_train_full[-5000:]
+
+
+def plot_image(image):
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+
+def show_reconstructions(model, images=X_valid, n_images=5):
+    reconstructions = model.predict(images[:n_images])
+    fig = plt.figure(figsize=(n_images * 1.5, 3))
+    for image_index in range(n_images):
+        plt.subplot(2, n_images, 1 + image_index)
+        plot_image(images[image_index])
+        plt.subplot(2, n_images, 1 + n_images + image_index)
+        plot_image(reconstructions[image_index])
+
+#grab sampling of latent space
+class Sampling(keras.layers.Layer):
+    def call(self, inputs):
+        mean, log_var = inputs
+        return K.random_normal(tf.shape(log_var)) * K.exp(log_var / 2) + mean 
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "autoencoders"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+tf.random.set_seed(42)
+np.random.seed(42)
+
+codings_size = 10
+
+inputs = keras.layers.Input(shape=[28, 28])
+z = keras.layers.Flatten()(inputs)
+z = keras.layers.Dense(150, activation="selu")(z)
+z = keras.layers.Dense(100, activation="selu")(z)
+codings_mean = keras.layers.Dense(codings_size)(z)
+codings_log_var = keras.layers.Dense(codings_size)(z)
+codings = Sampling()([codings_mean, codings_log_var])
+variational_encoder = keras.models.Model(
+    inputs=[inputs], outputs=[codings_mean, codings_log_var, codings])
+
+decoder_inputs = keras.layers.Input(shape=[codings_size])
+x = keras.layers.Dense(100, activation="selu")(decoder_inputs)
+x = keras.layers.Dense(150, activation="selu")(x)
+x = keras.layers.Dense(28 * 28, activation="sigmoid")(x)
+outputs = keras.layers.Reshape([28, 28])(x)
+variational_decoder = keras.models.Model(inputs=[decoder_inputs], outputs=[outputs])
+
+_, _, codings = variational_encoder(inputs)
+reconstructions = variational_decoder(codings)
+variational_ae = keras.models.Model(inputs=[inputs], outputs=[reconstructions])
+
+latent_loss = -0.5 * K.sum(
+    1 + codings_log_var - K.exp(codings_log_var) - K.square(codings_mean),
+    axis=-1)
+variational_ae.add_loss(K.mean(latent_loss) / 784.)
+variational_ae.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=[rounded_accuracy])
+history = variational_ae.fit(X_train, X_train, epochs=25, batch_size=128,
+                             validation_data=(X_valid, X_valid))
+
+show_reconstructions(variational_ae)
+plt.show()
